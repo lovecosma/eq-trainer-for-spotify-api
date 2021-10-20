fcfc7bd06bf6296f52fc9a04b4cf7a0a0ed7fc65
love.cosma <williamsmatthew333@gmail.com>
2021-10-20 00:07:35 -0600

    Refactoring for proxy on frontend.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  resources :albums
-  get '/login', to: "users#login"
-  post '/logout', to: "users#logout"
   get '/auth/spotify/callback', to: 'users#get_spotify_authorization'
   get '/get_info', to: "users#create_spotify_user"
-  get '/top_albums', to: "albums#top_albums"
-  namespace :api do
+  get '/login', to: "users#login"
+  scope :api do
+    get '/top_albums', to: "albums#top_albums"
+    post '/logout', to: "users#logout"
+    resources :albums
     resources :users 
   end 
 end
